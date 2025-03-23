065dd4fba58fbd2702256994eb5079357972fbcd
SkyHighTom <75322078+SkyHighTom@users.noreply.github.com>
2025-03-23 16:57:42 -0600

    Create Thomas_Stratford_u1415349.py

---
+++ Thomas_Stratford_u1415349.py
@@ -0,0 +1,52 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from collections import deque
+
+log = core.getLogger()
+
+class SimpleLoadBalancer(object):
+    def __init__(self, vip="10.0.0.100", servers=None):
+        self.vip = vip
+        self.servers = deque(servers) if servers else deque(["10.0.0.1", "10.0.0.2"])
+        self.mac_map = {}  # Maps IP addresses to MAC addresses
+        self.next_server = {}  # Tracks the next server for each client
+        core.openflow.addListeners(self)
+        log.info("Load Balancer initialized with VIP %s", self.vip)
+
+    def _handle_PacketIn(self, event):
+        packet = event.parsed
+        if not packet.parsed:
+            return
+        
+        in_port = event.port
+        ip_packet = packet.find("ipv4")
+        arp_packet = packet.find("arp")
+
+        if arp_packet and arp_packet.protodst == self.vip:
+            self.handle_arp_request(event, arp_packet, in_port)
+
+    def handle_arp_request(self, event, arp_packet, in_port):
+        client_ip = arp_packet.protosrc
+        if client_ip not in self.next_server:
+            self.next_server[client_ip] = self.servers[0]  # Assign next server
+            self.servers.rotate(-1)  # Rotate to next server
+        
+        target_ip = self.next_server[client_ip]
+        if target_ip in self.mac_map:
+            target_mac = self.mac_map[target_ip]
+        else:
+            return  # If MAC address is unknown, we wait
+        
+        arp_reply = of.ofp_packet_out()
+        arp_reply.actions.append(of.ofp_action_output(port=in_port))
+        arp_reply.data = event.data
+        event.connection.send(arp_reply)
+        log.info("Responded to ARP request: %s -> %s (%s)", client_ip, target_ip, target_mac)
+
+    def _handle_ConnectionUp(self, event):
+        log.info("Switch connected: %s", event.connection)
+        event.connection.addListeners(self)
+
+# Start the controller
+def launch():
+    core.registerNew(SimpleLoadBalancer)
