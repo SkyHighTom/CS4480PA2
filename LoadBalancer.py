d0c2702e79232d9f13bdce756c94fddd38f4f7eb
SkyHighTom <75322078+SkyHighTom@users.noreply.github.com>
2025-03-28 16:27:24 -0600

    Update LoadBalancer.py

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -48,8 +48,8 @@ def install_flow_rule(packet_type, client_port, server_port, dest_ip, connection
     msg2.match.nw_dst = IPAddr(f"10.0.0.{client_port}")
     msg2.match.nw_src = IPAddr(f"10.0.0.{server_port}")
     msg2.actions.append(of.ofp_action_dl_addr.set_src(getMac[IPAddr(f"10.0.0.{server_port}")]))
-    msg1.actions.append(of.ofp_action_nw_addr.set_src(IPAddr(f"10.0.0.{server_port}")))
-    msg1.actions.append(of.ofp_action_dl_addr.set_dst(getMac[IPAddr(f"10.0.0.{client_port}")]))
+    msg2.actions.append(of.ofp_action_nw_addr.set_src(IPAddr(f"10.0.0.{server_port}")))
+    msg2.actions.append(of.ofp_action_dl_addr.set_dst(getMac[IPAddr(f"10.0.0.{client_port}")]))
     msg2.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr(f"10.0.0.{client_port}")))
     msg2.actions.append(of.ofp_action_output(port=client_port))
 
