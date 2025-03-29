a9dddbdebf194bd86dc740542be8c93aad1219ce
SkyHighTom <75322078+SkyHighTom@users.noreply.github.com>
2025-03-29 10:42:07 -0600

    Update LoadBalancer.py

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -10,122 +10,99 @@ import pox.lib.recoco as recoco  # Multitasking library
 # Create a logger
 log = core.getLogger()
 
-# Static MAC and IP mappings
-getMac = {IPAddr(f"10.0.0.{i}"): EthAddr(f"00:00:00:00:00:0{i}") for i in range(1, 7)}
-getIPFromMac = {v: k for k, v in getMac.items()}
+class LoadBalancerController:
+    def __init__(self, connection):
+        self.connection = connection
+        connection.addListeners(self)
+        self.arp_table = {}
+        self.round_robin = {
+            IPAddr("10.0.0.5"): [IPAddr("10.0.0.5"), IPAddr("10.0.0.6")],
+            IPAddr("10.0.0.6"): [IPAddr("10.0.0.6"), IPAddr("10.0.0.5")]
+        }
+        self.flow_rules_installed = set()
 
-# Client and server IPs
-client_ips = [IPAddr(f"10.0.0.{i}") for i in range(1, 5)]
-server_ips = [IPAddr("10.0.0.5"), IPAddr("10.0.0.6")]
-current_server = 0
-round_robin = {IPAddr("10.0.0.5"): IPAddr("10.0.0.5"),
-               IPAddr("10.0.0.6"): IPAddr("10.0.0.6")}
-
-def _go_up(event):
-    log.info("Application up")
-
-def install_flow_rule(packet_type, client_port, server_port, dest_ip, connection):
-    """Installs bidirectional flow rules between a client and server."""
-    log.info(f"Installing flow rule: {client_port} <-> {server_port}")
+    def _handle_PacketIn(self, event):
+        packet = event.parsed
+        if not packet.parsed:
+            log.warning("Ignoring incomplete packet")
+            return
+        
+        if packet.type == pkt.ethernet.ARP_TYPE:
+            self._handleArp(event, packet)
+        elif packet.type == pkt.ethernet.IP_TYPE:
+            self._handleIP(event, packet)
     
-    # Client to Server
-    msg1 = of.ofp_flow_mod()
-    msg1.match.in_port = client_port
-    msg1.match.dl_type = packet_type
-    msg1.match.nw_dst = dest_ip
-    msg1.actions.append(of.ofp_action_dl_addr.set_src(getMac[IPAddr(f"10.0.0.{client_port}")]))
-    msg1.actions.append(of.ofp_action_nw_addr.set_src(IPAddr(f"10.0.0.{client_port}")))
-    msg1.actions.append(of.ofp_action_dl_addr.set_dst(getMac[IPAddr(f"10.0.0.{server_port}")]))
-    msg1.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr(f"10.0.0.{server_port}")))
-    msg1.actions.append(of.ofp_action_output(port=server_port))
-
-    connection.send(msg1)  # <-- Send the flow mod message to the switch!
-
-    # Server to Client
-    msg2 = of.ofp_flow_mod()
-    msg2.match.in_port = server_port
-    msg2.match.dl_type = packet_type
-    msg2.match.nw_dst = IPAddr(f"10.0.0.{client_port}")
-    msg2.match.nw_src = IPAddr(f"10.0.0.{server_port}")
-    msg2.actions.append(of.ofp_action_dl_addr.set_src(getMac[IPAddr(f"10.0.0.{server_port}")]))
-    msg2.actions.append(of.ofp_action_nw_addr.set_src(dest_ip))
-    msg2.actions.append(of.ofp_action_dl_addr.set_dst(getMac[IPAddr(f"10.0.0.{client_port}")]))
-    msg2.actions.append(of.ofp_action_nw_addr.set_dst(IPAddr(f"10.0.0.{client_port}")))
-    msg2.actions.append(of.ofp_action_output(port=client_port))
-
-    connection.send(msg2)  # <-- Send the rule
-
-def _handle_PacketIn(event):
-    global current_server
-    packet = event.parsed
-    if not packet.parsed:
-        log.warning("Ignoring incomplete packet")
-        return
-    log.info("PacketIn event")
-    
-    if packet.type == packet.ARP_TYPE:
+    def _handleArp(self, event, packet):
         arp_packet = packet.payload
-        actual_ip = arp_packet.protodst
-        log.info("ARP " + str(actual_ip))
-        if arp_packet.protodst not in round_robin:
-            round_robin[arp_packet.protodst] = server_ips[current_server % len(server_ips)]
-            current_server += 1
-        dest = round_robin[arp_packet.protodst]
+        vip = arp_packet.protodst
+        log.info(f"Handling ARP for {vip}")
         
-        if arp_packet.opcode == pkt.arp.REQUEST:
-            arp_reply = pkt.arp()
-            mac = getMac[dest]
-            arp_reply.hwsrc = mac
-            arp_reply.hwdst = packet.src
-            arp_reply.opcode = pkt.arp.REPLY
-            arp_reply.protosrc = getIPFromMac[mac]
-            arp_reply.protodst = arp_packet.protosrc
-            ether = pkt.ethernet()
-            ether.type = pkt.ethernet.ARP_TYPE  
-            ether.dst = packet.src
-            ether.src = mac
-            ether.payload = arp_reply
+        if vip in self.round_robin:
+            backend_ip = self.get_next_backend(vip)
+            mac_address = EthAddr(f"00:00:00:00:00:0{str(backend_ip)[-1]}")
+            self.arp_table[vip] = mac_address
+            self.send_arp_reply(event, packet, mac_address)
 
-            packet_out = of.ofp_packet_out()
-            packet_out.data = ether.pack()
-            packet_out.actions.append(of.ofp_action_output(port=event.port))
-            event.connection.send(packet_out)
-
-            client_port = int(str(arp_packet.protosrc)[-1])
-            server_port = int(str(dest)[-1])
-            install_flow_rule(pkt.ethernet.ARP_TYPE, client_port, server_port, actual_ip, event.connection)
-
-        elif arp_packet.opcode == pkt.arp.REPLY:
-            log.info("REPLY")
-    
-    elif packet.type == packet.IP_TYPE:
-        ip_packet = packet.payload
-        actual_ip = ip_packet.dstip
-        log.info("IP " + str(actual_ip))
-        if ip_packet.dstip not in round_robin:
-            round_robin[ip_packet.dstip] = server_ips[current_server % len(server_ips)]
-            current_server += 1
-        
-        backend_ip = round_robin[ip_packet.dstip]
-        client_port = int(str(ip_packet.srcip)[-1])
-        server_port = int(str(backend_ip)[-1])
-        
-        install_flow_rule(pkt.ethernet.IP_TYPE, client_port, server_port, actual_ip, event.connection)
-        
-        # Modify the packet for load balancing
-        ip_packet.dstip = backend_ip
+    def send_arp_reply(self, event, packet, mac_address):
+        arp_reply = pkt.arp()
+        arp_reply.hwsrc = mac_address
+        arp_reply.hwdst = packet.src
+        arp_reply.opcode = pkt.arp.REPLY
+        arp_reply.protosrc = packet.payload.protodst
+        arp_reply.protodst = packet.payload.protosrc
         ether = pkt.ethernet()
-        ether.type = pkt.ethernet.IP_TYPE
-        ether.src = getMac[ip_packet.srcip]
-        ether.dst = getMac[backend_ip]
-        ether.payload = ip_packet
-        
+        ether.type = pkt.ethernet.ARP_TYPE  
+        ether.dst = packet.src
+        ether.src = mac_address
+        ether.payload = arp_reply
+
         packet_out = of.ofp_packet_out()
         packet_out.data = ether.pack()
         packet_out.actions.append(of.ofp_action_output(port=event.port))
         event.connection.send(packet_out)
 
+    def _handleIP(self, event, packet):
+        ip_packet = packet.payload
+        vip = ip_packet.dstip
+        log.info(f"Handling IP packet for {vip}")
+        
+        if vip in self.round_robin:
+            backend_ip = self.get_next_backend(vip)
+            self.install_flow_rule(event, ip_packet.srcip, backend_ip)
+            self.forward_packet(event, packet, backend_ip)
+
+    def install_flow_rule(self, event, client_ip, backend_ip):
+        if backend_ip not in self.flow_rules_installed:
+            msg = of.ofp_flow_mod()
+            msg.match.nw_src = client_ip
+            msg.match.nw_dst = backend_ip
+            msg.actions.append(of.ofp_action_nw_addr.set_dst(backend_ip))
+            event.connection.send(msg)
+            self.flow_rules_installed.add(backend_ip)
+
+    def forward_packet(self, event, packet, backend_ip):
+        ip_packet = packet.payload
+        ip_packet.dstip = backend_ip
+        ether = pkt.ethernet()
+        ether.type = pkt.ethernet.IP_TYPE
+        ether.src = packet.src
+        ether.dst = EthAddr(f"00:00:00:00:00:0{str(backend_ip)[-1]}")
+        ether.payload = ip_packet
+
+        packet_out = of.ofp_packet_out()
+        packet_out.data = ether.pack()
+        packet_out.actions.append(of.ofp_action_output(port=event.port))
+        event.connection.send(packet_out)
+
+    def get_next_backend(self, vip):
+        backend_list = self.round_robin[vip]
+        backend = backend_list.pop(0)
+        backend_list.append(backend)  # Rotate backend selection
+        return backend
+
 @poxutil.eval_args
 def launch():
-    core.addListenerByName("UpEvent", _go_up)
-    core.openflow.addListenerByName("PacketIn", _handle_PacketIn)
+    def _go_up(event):
+        log.info("Connection up")
+        LoadBalancerController(event.connection)
+    core.addListenerByName("ConnectionUp", _go_up)
